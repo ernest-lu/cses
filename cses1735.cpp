a7248028f48b1d511cc6c5d9ea0f5dcc6830896f
Ernest <ernestlu.us@gmail.com>
2020-08-08 00:51:15 -0500

    news

--- cses1735.cpp
+++ cses1735.cpp
@@ -1,86 +1,115 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
-typedef pair<LL,LL> pii;
+typedef pair<int,int> pii;
 #define mp make_pair
 #define pb push_back
 #define f first
 #define s second
 
 
-struct nd{
-	LL t = 0;
-	pii as = mp(0, 0);
-	pii ss = mp(0, 0);
-};
-
 //var 
-LL n, q, a[200001];
-nd tr[800001];
+LL n, m, lza[800005], lzi[800005], tr[800005], pis[800005];
 
-void prop(int z, int l, int r){
-	int mid = (l + r)/2, sz1 = mid - l + 1, sz2 = r - (mid+1) +1, sz = sz1 + sz2;
-	if(tr[z].as.f > tr[z].ss.f){
-		tr[2*z].as = max(tr[2*z].as, tr[z].as);
-		tr[2*z + 1].as = max(tr[2*z + 1].as, tr[z].as);
-		tr[2*z].t = sz1 * tr[z].as.s;
-		tr[2*z+1].t = sz2 * tr[z].as.s;
+void psh(LL z, LL l, LL r){
+	if(pis[z]){
+		tr[z] = (r-l+1) * lza[z];
+		if(l!=r){
+			lza[2*z] = lza[z]; pis[2*z] = 1;
+			lza[2*z+1] = lza[z]; pis[2*z+1] = 1;
+			lzi[2*z] = 0; lzi[2*z+1] = 0;
+		}
 	}
-
 	else{
-
+		tr[z] += ((r-l+1) * lzi[z]);
+		if(l != r){
+			if(pis[2*z]) lza[2*z] += lzi[z];
+			else lzi[2*z] += lzi[z];
+			if(pis[2*z+1]) lza[2*z+1] += lzi[z];
+			else lzi[2*z+1] += lzi[z];
+		}
 	}
 
-	tr[z].as = mp(0, 0);
-	tr[z].ss = mp(0, 0);
+	pis[z] = 0; lza[z] = 0; lzi[z] = 0;
+	return;
 }
 
-void psh(int a, int b, int z, int l, int r){
-	if(r < a || l > b) return;
-	if(l >= a && r <= b) return;
-	prop(z, l, r);
-	int mid = (l + r)/2;
-	psh(a, b, 2*z, l, mid);
-	psh(a, b, 2*z+1, mid+1, r);
-}
-
-void ass(int a, int b, pii o, int z, int l, int r){
-	if(r < a || l > b) return;
-	if(l >= a && r <= b){
-		tr[z].t = (o.s) * (r - l + 1);
-		tr[z].as = max(o, tr[z].as);
+void upa(LL z, LL l, LL r, LL lb, LL rb, LL g){
+	psh(z, l, r);
+	if(l > rb || r < lb) return;
+	if(l >= lb && r <= rb){
+		lzi[z] = 0;
+		lza[z] = g;
+		pis[z] = 1;
+		psh(z,l,r);
 		return;
-	} 
+	}
 
-	int mid = (l + r)/2;
-	ass(a, b, o, 2*z, l, mid);
-	ass(a, b, o, 2*z+1, mid+1, r);
-
-	tr[z].t = tr[2*z].t + tr[2*z+1].t;
-
+	LL mid = (l+r)/2;
+	upa(2*z, l, mid, lb, rb, g);
+	upa(2*z+1, mid+1, r, lb, rb, g);
+	tr[z] = tr[2*z] + tr[2*z+1];
 }
 
-void iss(int a, int b, pii o, int z, int l, int r){
-	if(r < a || l > b) return;
-	if(l >= a && r <= b){
-		tr[z].t += (o.s) * (r - l + 1);
-		tr[z].ss.f = o.f; tr[z].ss.s += o.s;
+void upi(LL z, LL l, LL r, LL lb, LL rb, LL g){
+	psh(z, l, r);
+	if(l > rb || r < lb) return;
+	if(l >= lb && r <= rb){
+		if(pis[z]){
+			lza[z] += g;
+			psh(z, l, r);
+		}
+		else{
+			lzi[z] += g;
+			psh(z, l, r);
+		}
 		return;
-	} 
+	}
 
-	int mid = (l + r)/2;
-	iss(a, b, o, 2*z, l, mid);
-	iss(a, b, o, 2*z+1, mid+1, r);
+	LL mid = (l + r)/2;
+	upi(2*z, l, mid, lb, rb, g);
+	upi(2*z+1, mid+1, r, lb, rb, g);
+	tr[z] = tr[2*z] + tr[2*z+1];
+}
 
-	tr[z].t = tr[2*z].t + tr[2*z+1].t;
+LL ge(LL z, LL l, LL r, LL lb, LL rb){
+	psh(z, l, r);
+	if(l > rb || r < lb) return 0;
+	if(l >= lb && r <= rb)
+		return tr[z];
 
+	LL mid = (l + r)/2;
+	return ge(2*z, l, mid, lb, rb) + ge(2*z+1, mid+1, r, lb, rb);
 }
 
 int main(){
 	ios_base::sync_with_stdio(0); 
 	cin.tie(0);
 
+	cin >> n >> m;
+	for(int i = 1; i<=n; i++){
+		LL t1;
+		cin >> t1;
+		upa(1,1,n,i,i,t1);
+	}
 
+	while(m--){
+		LL tt, l, r, v;
+		cin >> tt;
+		if(tt == 1){
+			cin >> l >> r >> v;
+			upi(1,1,n,l,r,v);
+			continue;
+		}
+		if(tt == 2){
+			cin >> l >> r >> v;
+			upa(1,1,n,l,r,v);
+			continue;
+		}
+
+		cin >> l >> r;
+		cout << ge(1,1,n,l,r) << endl;
+	}
 
 	cout.flush();
 	return 0;
